ad77a190d2e3969216452c70eeb1fefee4a93a77
Ekakshar Shreyas <shreyasekakshar1605@gmail.com>
2026-07-07 20:50:21 +0530

    bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -69,7 +69,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     return res.status(201).json(
-        new ApiResponse(200, createdUser, "User registered Successfully");
+        new ApiResponse(200, createdUser, "User registered Successfully")
     )
 
 
